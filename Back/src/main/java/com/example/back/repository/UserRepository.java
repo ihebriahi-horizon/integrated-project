34accb864fe22cb44978751fed3f925fa86bed35
Iheb RIAHI <iriahi@mobelite.fr>
2023-11-03 14:57:55 +0100

    Added login & register in backend using jwt. Added login interface.

---
+++ Back/src/main/java/com/example/back/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.back.repository;
+
+import org.horizon.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	Optional<User> findByUserEmail(String email);
+
+    Boolean existsByUserEmail(String email);
+}
