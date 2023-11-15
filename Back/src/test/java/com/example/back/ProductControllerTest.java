c21b3feb95dfa38869351ecf65a3cf117f174086
jnyoussef <youssef.jneyah@horizon-tech.tn>
2023-11-15 23:43:31 +0100

    SetUp for tests

--- Back/src/test/java/com/example/back/ProductControllerTest.java
+++ Back/src/test/java/com/example/back/ProductControllerTest.java
@@ -1,5 +1,19 @@
 package com.example.back;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.example.back.controller.ProductController;
+import com.example.back.service.ProductService;
+
 public class ProductControllerTest {
-    
+    private ProductController productController;
+    @Mock
+    private ProductService productService;
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        productController = new ProductController(productService);
+    }
 }
