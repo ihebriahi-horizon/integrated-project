890d4f53e61545bbb8f82a31950cb2ed6a791b04
jnyoussef <youssef.jneyah@horizon-tech.tn>
2023-11-15 23:52:17 +0100

    test for GetProducts page

--- Back/src/test/java/com/example/back/ProductControllerTest.java
+++ Back/src/test/java/com/example/back/ProductControllerTest.java
@@ -1,10 +1,18 @@
 package com.example.back;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
 
 import com.example.back.controller.ProductController;
+import com.example.back.entity.Product;
 import com.example.back.service.ProductService;
 
 public class ProductControllerTest {
@@ -15,5 +23,17 @@ public class ProductControllerTest {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         productController = new ProductController(productService);
+    }
+     @Test
+    public void testGetProducts() {
+        int page = 0;
+        int size = 0;
+        Page<Product> expectedPage = mock(Page.class);
+        when(productService.getProducts(page, size)).thenReturn(expectedPage);
+
+        Page<Product> actualPage = productController.getProducts(page, size);
+
+        assertEquals(expectedPage, actualPage);
+        verify(productService).getProducts(page, size);
     }
 }
