7151290360fa0f3a758e71e67ccd9abe54d08040
jnyoussef <youssef.jneyah@horizon-tech.tn>
2023-11-15 23:03:03 +0100

    test for GetUser

--- Back/src/test/java/com/example/back/UserControllerTest.java
+++ Back/src/test/java/com/example/back/UserControllerTest.java
@@ -1,5 +1,42 @@
 package com.example.back;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.example.back.controller.UserController;
+import com.example.back.entity.User;
+import com.example.back.service.UserService;
+
 public class UserControllerTest {
     
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        userController = new UserController(userService);
+    }
+
+    @Test
+    public void testGetUser() {
+        int userId = 1;
+        User expectedUser = mock(User.class);
+        when(userService.getUser(userId)).thenReturn(expectedUser);
+
+        User actualUser = userController.getUser(userId);
+
+        assertEquals(expectedUser, actualUser);
+        verify(userService).getUser(userId);
+    }
+
 }
